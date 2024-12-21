82986e69d21647b3591a72347d7efbc27ead8d7e
thdihan <thdihan@outlook.com>
2024-12-21 20:07:10 +0600

    Recipe type added

--- Challenge 2 Folder/src/app/modules/recipes/recipes.interface.ts
+++ Challenge 2 Folder/src/app/modules/recipes/recipes.interface.ts
@@ -3,6 +3,7 @@ import { TIngredient } from "../ingredients/ingredients.interface";
 export type TRecipe = {
     id: string;
     name: string;
+    type: string;
     description: string;
     imagePath: string;
     ingredients: TIngredient[];
