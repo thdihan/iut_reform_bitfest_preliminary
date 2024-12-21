add64219dee232fe3f4749b47d11daba927ee254
thdihan <thdihan@outlook.com>
2024-12-21 21:48:51 +0600

    Readme added done

--- Challenge 2 Folder/src/app/modules/recipes/recipes.routes.ts
+++ Challenge 2 Folder/src/app/modules/recipes/recipes.routes.ts
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get("/", RecipiesController.getRecipies);
 router.post("/", RecipiesController.addRecipe);
-router.get("/suggestions", RecipiesController.getSuggestions);
+router.post("/suggestions", RecipiesController.getSuggestions);
 
 export const RecipiesRoutes = router;
